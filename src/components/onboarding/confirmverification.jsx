fe1f1e96693ff1698263112b41209eafb440e4d8
yashPratp983 <yash.here07@gmail.com>
2023-02-11 00:05:22 +0530

    rectified a bug

--- src/components/onboarding/confirmverification.jsx
+++ src/components/onboarding/confirmverification.jsx
@@ -1,15 +1,17 @@
+
+
 import { useNavigate } from "react-router";
 import classes from './confirmverification.module.css';
-const WrongUrl = () => {
+const confirmverification = () => {
     const navigate = useNavigate();
 
     return (
         <div className={classes.parent}>
-            <h1 className={classes.heading}>Invalid Token</h1>
-            <p className={classes.para}>Your token is invalid.</p>
-            <button className={classes.button} onClick={() => { navigate('/login') }}>Sign in</button>
+            <h1 className={classes.heading}>Email Verified!!</h1>
+            <p className={classes.para}>Congratulations your email has been successfully verified.Please click on next button to proceed.</p>
+            <button className={classes.button} onClick={() => { navigate('/uploadphoto') }}>Next</button>
         </div>
     );
 }
 
-export default WrongUrl;
+export default confirmverification;
